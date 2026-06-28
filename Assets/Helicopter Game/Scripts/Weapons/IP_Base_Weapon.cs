afc96bba2b0400677a24f38c4c424d9b5487d260
WadeWilson228 <69381493+vadimvatsenko@users.noreply.github.com>
2026-06-28 14:31:09 +0300

    Coding the Base Weapon Script

--- Assets/Helicopter Game/Scripts/Weapons/IP_Base_Weapon.cs
+++ Assets/Helicopter Game/Scripts/Weapons/IP_Base_Weapon.cs
@@ -1,14 +1,54 @@
 using Helicopter_Game.Scripts.Weapons;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 public class IP_Base_Weapon : MonoBehaviour, IP_IWeapon
 {
+    [Header("Base Weapon Property")] 
+    [SerializeField] private bool allowFiring = true;
+    [SerializeField] private Transform muzzlePos;
+    [SerializeField] private GameObject projectilePrefab;
+    [SerializeField] private int maxAmmoCount = 100;
+    [Space(5)]
+    [SerializeField] private GameObject muzzleFlashPrefab;
+    
+    protected AudioSource AudioSource;
+    protected int CurrentAmmo = 0;
+
+    private void Start()
+    {
+        CurrentAmmo = maxAmmoCount;
+    }
     public void FireWeapon()
+    {
+        if (CurrentAmmo > 0)
+        {
+            HandleProjectile();
+            HandleAudioSource();
+            HandleVFX();
+            
+            CurrentAmmo--;
+            CurrentAmmo = Mathf.Clamp(CurrentAmmo, 0, maxAmmoCount);
+        }
+        else
+        {
+            Reload();
+        }
+    }
+
+    public void Reload() => CurrentAmmo = maxAmmoCount;
+    
+    protected virtual void HandleProjectile()
     {
         throw new System.NotImplementedException();
     }
-
-    public void Reload()
+    
+    protected virtual void HandleAudioSource()
+    {
+        throw new System.NotImplementedException();
+    }
+    
+    protected virtual void HandleVFX()
     {
         throw new System.NotImplementedException();
     }
--- Assets/Helicopter Game/Scripts/Weapons/IP_HeliWeapon_Controller.cs
+++ Assets/Helicopter Game/Scripts/Weapons/IP_HeliWeapon_Controller.cs
@@ -4,6 +4,8 @@ namespace Helicopter_Game.Scripts.Weapons
 {
     public class IP_HeliWeapon_Controller : MonoBehaviour
     {
+        [Header("Weapon Conteroller Propperties")]
+        [SerializeField] public bool allowFiring = true;
         // Start is called once before the first execution of Update after the MonoBehaviour is created
         void Start()
         {
